0507f5a135db8de484dabfba0edd411f03942f1e
joonhyong <dbshwns0528@naver.com>
2026-02-17 05:50:26 +0900

    [HSC-93] style: 글자 크기 통일

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <div className="flex min-h-screen items-center justify-center">customer-web</div>;
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      {/* customer-web */}
+      <div className="font-display flex flex-col">
+        <p className="font-display2 text-3xl">test1</p>
+        <p className="font-sans text-2xl">test2</p>
+        <p className="text-xl">test3</p>
+      </div>
+    </div>
+  );
 }

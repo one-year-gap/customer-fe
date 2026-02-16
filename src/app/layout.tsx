873fba0822eee6dbe5411a005ac3f7ea324a3a12
joonhyong <dbshwns0528@naver.com>
2026-02-17 05:48:17 +0900

    [HSC-93] style: 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,34 @@
 import "./globals.css";
 
+import localFont from "next/font/local";
+
 import { TanstackProvider } from "@/lib/tanstack/provider";
+
+const pretendard = localFont({
+  src: "../assets/fonts/PretendardVariable.woff2",
+  weight: "100 900",
+  variable: "--font-pretendard",
+  display: "swap",
+});
+
+const dunggeunmo = localFont({
+  src: "../assets/fonts/DungGeunMo.woff2",
+  weight: "400",
+  variable: "--font-dunggeunmo",
+  display: "swap",
+});
+
+const mulmaru = localFont({
+  src: "../assets/fonts/MulMaru.woff2",
+  weight: "400",
+  variable: "--font-mulmaru",
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <body>
+      <body className={`${pretendard.variable} ${dunggeunmo.variable} ${mulmaru.variable}`}>
         <TanstackProvider>{children}</TanstackProvider>
       </body>
     </html>

ca59a564843ece227c7174ddf8d2386915c9a202
Zoo2-bi <bagjui068@gmail.com>
2026-03-18 14:34:20 +0900

    [HSC-358] fix: 오류수정

--- src/app/(service)/products/page.tsx
+++ src/app/(service)/products/page.tsx
@@ -1,9 +1,13 @@
 "use client";
 
-export const dynamic = "force-dynamic";
+import { Suspense } from "react";
 
 import ProductsPageInner from "./ProductsPageInner";
 
 export default function ProductsPage() {
-  return <ProductsPageInner />;
+  return (
+    <Suspense fallback={<div>로딩중...</div>}>
+      <ProductsPageInner />
+    </Suspense>
+  );
 }
